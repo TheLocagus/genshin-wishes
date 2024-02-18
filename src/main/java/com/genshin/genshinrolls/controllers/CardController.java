9752d0ff812d3a78ecd6c5b73e4d7add76e88c1a
TheLocagus <bartek_kolsut1@wp.pl>
2024-02-18 16:38:35 +0100

    Corrected dto, started implementing ten-roll service

---
+++ src/main/java/com/genshin/genshinrolls/controllers/CardController.java
@@ -0,0 +1,44 @@
+package com.genshin.genshinrolls.controllers;
+
+import com.genshin.genshinrolls.entity.CardEntity;
+import com.genshin.genshinrolls.service.CardServiceImpl;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class CardController {
+
+    private CardServiceImpl cardService;
+
+    public CardController(CardServiceImpl cardService) {
+        this.cardService = cardService;
+    }
+
+    @GetMapping("/")
+    ArrayList<CardEntity> getAllCards(){
+        return this.cardService.getAll();
+    }
+
+    @PostMapping("/")
+    CardEntity save(@RequestBody CardEntity card){
+        return this.cardService.save(card);
+    }
+
+    @PutMapping("/{id}")
+    CardEntity update(@RequestBody CardEntity card, @PathVariable Long id){
+        return this.cardService.update(card, id);
+    }
+
+    @DeleteMapping("/{id}")
+    CardEntity delete(@PathVariable Long id){
+        return this.cardService.delete(id);
+    }
+
+    @GetMapping("/ten-roll")
+    CardEntity[] getTenCards(){
+        return this.cardService.getTen();
+    }
+
+
+}
